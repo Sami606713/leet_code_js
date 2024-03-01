94bed074a6b1dc5fe3acfd65ab26db308584a603
samiullah <sami606713@gmail.com>
2024-03-01 10:04:31 +0500

    two_sum2 problem

--- two_sum2.js
+++ two_sum2.js
@@ -1,14 +1,29 @@
 let arr=[1,2,3,4,5]
 let target=9
-var twoSum = function(nums, target) {
-    num_dic={}
-    for(let i=0;i<nums.length;i++){
-        let complement=target-nums[i]
-        if(complement in num_dic){
-            return [num_dic[complement]+1,i+1]
-        }
-        num_dic[nums[i]]=i
-    }
-};
+// var twoSum = function(nums, target) {
+//     num_dic={}
+//     for(let i=0;i<nums.length;i++){
+//         let complement=target-nums[i]
+//         if(complement in num_dic){
+//             return [num_dic[complement]+1,i+1]
+//         }
+//         num_dic[nums[i]]=i
+//     }
+// };
 
-console.log(twoSum(arr,target));
+// console.log(twoSum(arr,target));
+
+let dic={}
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    let complement=target-arr[i]
+    console.log(complement);
+    if(complement in dic){
+        console.log([dic[complement]+1,i+1]);
+    }else{
+        dic[arr[i]]=i
+    }
+
+
+    
+}
